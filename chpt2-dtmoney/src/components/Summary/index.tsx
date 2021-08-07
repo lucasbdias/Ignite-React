e0733476af6e2e1cdc915f198f16bef442a61a2a
Lucas Braz <lucasbrzdias@gmail.com>
2021-08-07 11:04:30 -0300

    feat: 🎸 summary calculation

--- chpt2-dtmoney/src/components/Summary/index.tsx
+++ chpt2-dtmoney/src/components/Summary/index.tsx
@@ -10,7 +10,22 @@ import * as S from './styles';
 
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
-  console.log(transactions)
+
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'deposit') {
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
+    } else {
+      acc.withdraws += transaction.amount;
+      acc.total -= transaction.amount;
+    }
+
+    return acc;
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0,
+  });
 
   return (
     <S.Container>
@@ -19,21 +34,36 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong>R$69,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.deposits)}
+        </strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Entradas" />
         </header>
-        <strong>- R$24,00</strong>
+        <strong>
+        {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.withdraws)}
+        </strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Entradas" />
         </header>
-        <strong>R$45,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.total)}
+        </strong>
       </div>
     </S.Container>
   );
